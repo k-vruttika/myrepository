d8237ee8e9722adb87fe5e8df4312c67ba312827
k-vruttika <148611124+k-vruttika@users.noreply.github.com>
2024-01-30 14:48:22 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
-
-int main() {
-    int num66, num77, sum;
-    
-    printf("Enter the first number: ");
-    scanf("%d", &num66);
-    
-    printf("Enter the second number: ");
-    scanf("%d", &num77);
-    
-    sum = num66 + num77;
-    
-    printf("The sum of %d and %d is %d.", num66, num77, sum);
-    
-    return 0;
+#include <stdio.h> 
+int main() 
+{ 
+    int A, B, sum = 0; 
+    printf("Enter two numbers A and B : \n"); 
+    scanf("%d%d", &A, &B); 
+    sum = A + B; 
+    printf("Sum of A and B is: %d", sum); 
+    return 0; 
 }
